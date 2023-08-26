5d099fb8169d1a63490de5fa65f9134ad7538b8b
jainikhs <jainnikh3@gmail.com>
2023-08-26 23:16:59 +0530

    add some files of terraform

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,13 +1,11 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "my access key"
-  secret_key = "my secret key"
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "mynewec2" {
     ami = "ami-08e5424edfe926b43"
     instance_type = "t2.micro"
-    tags = {
+    tags = { 
         Name = "my-first-ec2"
     }
 }
